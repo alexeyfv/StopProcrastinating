dda2750663f388a6a81190da57799107aac6952c
Alexey Fedorov <alexey.fedorov.dev@gmail.com>
2021-03-03 00:23:53 +0300

    Change Android apps manager functionality

--- StopProcrastinating/StopProcrastinating.Android/Services/AppsManager/AppsManager.cs
+++ StopProcrastinating/StopProcrastinating.Android/Services/AppsManager/AppsManager.cs
@@ -1,11 +1,12 @@
-﻿using Android.Content.PM;
+﻿using Android.App;
+using Android.App.Usage;
+using Android.Content;
 using StopProcrastinating.Droid.Services.AppsManager;
 using StopProcrastinating.Interfaces.AppsManager;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using Xamarin.Forms;
 
-[assembly: Dependency(typeof(AppsManager))]
+[assembly: Xamarin.Forms.Dependency(typeof(AppsManager))]
 namespace StopProcrastinating.Droid.Services.AppsManager
 {
     public class AppsManager : IAppsManager
@@ -14,10 +15,40 @@ namespace StopProcrastinating.Droid.Services.AppsManager
 
         public IEnumerable<IApp> GetInstalledApps()
         {
+            var list = new List<IApp>();
 
-            var apps = Android.App.Application.Context.PackageManager.GetInstalledApplications(PackageInfoFlags.MatchAll);
+            try
+            {
+                var usageStatsManager = Application.Context.GetSystemService(Context.UsageStatsService) as UsageStatsManager;
 
-            return apps.Select(app => new App(app.Name ?? string.Empty));
+                long endTime = TimeConverter.Convert(DateTime.UtcNow);
+                long beginTime = endTime - 1000 * 10;
+
+
+                var stats = usageStatsManager.QueryUsageStats(UsageStatsInterval.Weekly, beginTime, endTime);
+
+                foreach (var item in stats)
+                {
+                    list.Add(new App($"App: {item.PackageName ?? string.Empty}; T: {TimeConverter.Convert(item.LastTimeUsed)}"));
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
+            return list;
+        }
+
+        class TimeConverter
+        {
+            private static DateTime StartTime { get; } = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            public static long Convert(DateTime time) => 
+                (long)(time - StartTime).TotalMilliseconds;
+
+            public static DateTime Convert(long time) => StartTime.AddMilliseconds(time);
         }
 
         #endregion
